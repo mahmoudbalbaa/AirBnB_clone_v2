fb6c8f5d703d3a22e979be1246cbe3ab865938e5
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2024-03-09 04:09:54 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
-# Script to set up web servers for deployment of 'web_static' project
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '37i\\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
