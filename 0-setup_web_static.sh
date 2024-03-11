97ecfb118b9f25988c05cd1daf1d42ea3da079fb
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2024-03-10 18:05:37 -0700

    1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,11 +11,11 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
